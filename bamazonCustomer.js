1b22fedbe6a1224222ff02b0e6dab95a3218f438
Jamison Gordon <jgordon@uhin.org>
2019-02-24 12:12:28 -0700

    Added logic to display current stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,8 @@
 var mysql = require('mysql');
+const cTable = require('console.table');
 require('dotenv').config();
+
+let products = [];
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -13,11 +16,27 @@ connection.connect(function(err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
- 
-  console.log('connected as id ' + connection.threadId);
 });
  
-// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-//   if (error) throw error;
-//   console.log('The solution is: ', results[0].solution);
-// });
+connection.query('SELECT * FROM products', function (error, results, fields) {
+  // When done with the connection, release it.
+
+  for(product of results) {
+    products.push([
+      product.item_id,
+      product.product_name,
+      product.department_name,
+      product.price,
+      product.stock_quantity
+    ]);
+  }
+  console.log("\n\nCurrent Stock:\n");
+  console.table(['item_id', 'product_name', 'department_name', 'price', 'stock_quantity'], products);
+
+  // Handle error after the release.
+  if (error) throw error;
+
+  // Don't use the connection here, it has been returned to the pool.
+});
+
+connection.end();
